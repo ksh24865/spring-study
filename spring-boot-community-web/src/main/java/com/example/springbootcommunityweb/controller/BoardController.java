06d679afbefd5a8785ed1a68b37a9e6910d1da12
ksh24865 <8819889@naver.com>
2021-05-05 03:34:31 +0900

    complete chap4

--- spring-boot-community-web/src/main/java/com/example/springbootcommunityweb/controller/BoardController.java
+++ spring-boot-community-web/src/main/java/com/example/springbootcommunityweb/controller/BoardController.java
@@ -21,14 +21,14 @@ public class BoardController {
     public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model){
         // RequestParam idx파라미터 필수로 받음, 기본값 0
         model.addAttribute("board", boardService.findBoardByIdx(idx));
-        return "/board/form";
+        return "board/form";
     }
 
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model){
-        // PageableDefault 어노테이션을 사용하여 페이징 처리에 대한 규약 정의
+        // PageableDefault 어노테이션을 사용하여 페이징 처리에 대한 규약 정의한 pageable을 findBoardList에게 넘겨
         model.addAttribute("boardList", boardService.findBoardList(pageable));
-        return "/board/list";
+        return "board/list";
 
     }
 
--- spring-boot-community-web/src/main/java/com/example/springbootcommunityweb/service/BoardService.java
+++ spring-boot-community-web/src/main/java/com/example/springbootcommunityweb/service/BoardService.java
@@ -20,6 +20,8 @@ public class BoardService {
         // pageale로 넘어온 pageNumber 객체가 0이하일 때 0으로 초기화,
         // 기본 페이지 크기인 10으로 새로운 pageRequest객체를 만들어 페이징 처리된 게시글 리스트 반환
         // DB에 저장된 Entity들을 페이지로 나누는 것
+        // PageRequest.of(page 검색을 원하는 페이지 번호, size 한 페이지 당 조회할 개시물 개수, sort 정렬방식)
+        // 0페이지부터 페이지 사이즈 만큼 검색 (전부)
         pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 :
                 pageable.getPageNumber() -1, pageable.getPageSize());
         return boardRepository.findAll(pageable);
