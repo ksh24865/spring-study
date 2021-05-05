08e53c05fe32ff51ffa1b2514c28c7d0adf31420
ksh24865 <8819889@naver.com>
2021-05-06 04:03:48 +0900

    add SocialType

---
+++ spring-boot-community-web/src/main/java/com/example/springbootcommunityweb/domain/enums/SocialType.java
@@ -0,0 +1,26 @@
+package com.example.springbootcommunityweb.domain.enums;
+
+import java.util.Locale;
+
+public enum SocialType {
+    FACEBOOK("facebook"),
+    GOOGLE("google"),
+    KAKAO("kakao");
+
+    private final String ROLE_PREFIX = "ROLE_";
+    private String name;
+
+    SocialType(String name) {
+        this.name = name;
+    }
+
+    public String getRoleType() {return ROLE_PREFIX + name.toUpperCase();}
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEquals(String authority){
+        return this.getRoleType().equals(authority);
+    }
+}
