5a20fa595dcb354f649a013289aedac2184ce617
Seongho <55729930+ksh24865@users.noreply.github.com>
2021-07-10 22:03:30 +0900

    Update FileUpDownloadTutorialApplication.java

--- fileUpDownloadTutorial/src/main/java/com/fileUpDownloadTutorial/FileUpDownloadTutorialApplication.java
+++ fileUpDownloadTutorial/src/main/java/com/fileUpDownloadTutorial/FileUpDownloadTutorialApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FileUpDownloadTutorialApplication {
-
+	// https://spring.io/guides/gs/uploading-files/
 	public static void main(String[] args) {
 		SpringApplication.run(FileUpDownloadTutorialApplication.class, args);
 	}
